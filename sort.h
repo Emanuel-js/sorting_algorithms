e22f37904c87f44ab119701162924139a2f3a12d
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-06-13 11:02:47 +0300

    Function Prototype

--- sort.h
+++ sort.h
@@ -1,5 +1,9 @@
-#ifndef BIGO_H
-#define BIGO_H
+#ifndef _SORTING_H_
+#define _SORTING_H_
+#include <stdio.h>
+
+
+/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -10,10 +14,22 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+/** Given FUN prototype */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/** TASK Prototype */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
